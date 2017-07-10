a3753c30bf27d54d519ea41fcd2286dcd5be09c6
JoaoHergert <joaohergert@gmail.com>
2017-07-10 15:25:12 -0300

    getting updates

--- LocalCommerce/SearchFilterViewController.swift
+++ LocalCommerce/SearchFilterViewController.swift
@@ -10,11 +10,95 @@ import UIKit
 
 class SearchFilterViewController: UIViewController {
     
+   
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let insets = UIEdgeInsetsMake(20.0, 0.0, 0.0, 0.0)
+        scrollView.contentInset = insets
+        scrollView.scrollIndicatorInsets = insets
+        
+        let stack = stackView
+        let index = (stack?.arrangedSubviews.count)! - 1
+        let addView = stack?.arrangedSubviews[index]
+        
+        let scroll = scrollView
+        let offset = CGPoint(x: (scroll?.contentOffset.x)!,
+                             y: (scroll?.contentOffset.y)! + (addView?.frame.size.height)!)
+        
+        let newView = createEntry()
+        newView.isHidden = true
+        stack?.insertArrangedSubview(newView, at: index)
+        
+        UIView.animate(withDuration: 0.25) { () -> Void in
+            newView.isHidden = false
+            scroll?.contentOffset = offset
+        }
+
+        
+        
     }
+    
+    
+    
+    // MARK: - Private Methods
+    private func createEntry() -> UIView {
+        let date = DateFormatter.localizedString(from: NSDate() as Date, dateStyle: .short, timeStyle: .none)
+        let number = "\(randomHexQuad())-\(randomHexQuad())-\(randomHexQuad())-\(randomHexQuad())"
+        
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.alignment = .firstBaseline
+        stack.distribution = .fill
+        stack.spacing = 8
+        
+        let dateLabel = UILabel()
+        dateLabel.text = date
+        dateLabel.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
+        
+        let numberLabel = UILabel()
+        numberLabel.text = number
+        numberLabel.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
+        
+        let deleteButton = UIButton(type: .roundedRect)
+        deleteButton.setTitle("Delete", for: .normal)
+        deleteButton.addTarget(self, action: #selector(deleteStackView(sender: )), for: .touchUpInside)
+        
+        stack.addArrangedSubview(dateLabel)
+        stack.addArrangedSubview(numberLabel)
+        stack.addArrangedSubview(deleteButton)
+        
+        return stack
+    }
+    
+    private func randomHexQuad() -> String {
+        return NSString(format: "%X%X%X%X",
+                        arc4random() % 16,
+                        arc4random() % 16,
+                        arc4random() % 16,
+                        arc4random() % 16
+            ) as String
+    }
+
+
+    
+    func deleteStackView(sender: UIButton) {
+        if let view = sender.superview {
+            UIView.animate(withDuration: 0.25, animations: { () -> Void in
+                view.isHidden = true
+            }, completion: { (success) -> Void in
+                view.removeFromSuperview()
+            })
+        }
+    }
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -22,23 +106,23 @@ class SearchFilterViewController: UIViewController {
     }
 }
 
-    extension SearchFilterViewController: UITableViewDelegate, UITableViewDataSource {
-        
-        func numberOfSections(in tableView: UITableView) -> Int {
-            return 1
-        }
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return array.count
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "filter", for: indexPath) as! SearchFilterTableViewCell
-            
-            cell.category.text = "TsT"
-            
-            
-            return cell
-        }
-        
-    }
+//    extension SearchFilterViewController: UITableViewDelegate, UITableViewDataSource {
+//        
+//        func numberOfSections(in tableView: UITableView) -> Int {
+//            return 1
+//        }
+//        
+//        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//            return array.count
+//        }
+//        
+//        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "filter", for: indexPath) as! SearchFilterTableViewCell
+//            
+//            cell.category.text = "TsT"
+//            
+//            
+//            return cell
+//        }
+//        
+//    }
