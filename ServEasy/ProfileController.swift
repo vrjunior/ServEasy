39dfe83bee1db03c998c0b081eba265b18e14c20
Valmir Jr <valmirmassoni@gmail.com>
2017-07-23 21:30:18 -0300

    making profile storyboard

--- ServEasy/ProfileController.swift
+++ ServEasy/ProfileController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ProfileController: UIViewController {
+class ProfileController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var editProfilePicture: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.imagePicker.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,49 @@ class ProfileController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onTapEditButton(_ sender: Any) {
+        if(self.tableView.isEditing) {
+            self.tableView.setEditing(false, animated: true)
+            self.editButton.setTitle("EDIT".localized, for: .normal)
+            self.editProfilePicture.isHidden = true
+        }
+        else {
+            self.tableView.setEditing(true, animated: true)
+            self.editButton.setTitle("DONE".localized, for: .normal)
+            self.editProfilePicture.isHidden = false
+        }
+    }
+    @IBAction func onTapEditProfilePictureButton(_ sender: UIButton) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "TAKE_PHOTO".localized, style: .default, handler: takeAProfiePhoto))
+        actionSheet.addAction(UIAlertAction(title: "CHOOSE_PHOTO".localized, style: .default, handler: chooseAProfilePhoto))
+        actionSheet.addAction(UIAlertAction(title: "CANCEL".localized, style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func takeAProfiePhoto(alert: UIAlertAction!) {
+        self.imagePicker.sourceType = .camera
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }
+    func chooseAProfilePhoto(alert: UIAlertAction!) {
+        self.imagePicker.sourceType = .photoLibrary
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }
+    
+    private func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            //updating image
+            self.profileImage.image = pickedImage
+            
+            //update on server
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
     
 }
-
